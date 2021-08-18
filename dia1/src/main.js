64fb33208d07937934b4249413054a0380b398a9
Gabriel Ramos <dev.gabrielramos@gmail.com>
2021-08-18 08:38:23 -0300

    Dia 01 - Resolução do exercício 03

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,8 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
+const divApp = document.querySelector('[data-js="app"]')
+
+divApp.innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
