dcbcb9a040961f7a1b3f0ba0e24269a97442f7d1
Gabriel Ramos <dev.gabrielramos@gmail.com>
2021-08-18 08:46:24 -0300

    Dia 01 - Resolução do exercício 04

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,8 +1,19 @@
 import './style.css'
 
 const divApp = document.querySelector('[data-js="app"]')
+const link = document.querySelector('[data-js="link"]')
 
 divApp.innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+
+const handleToggleVisibilityDivApp = event => {
+  const divAppIsHidden = divApp.classList.toggle('is-hidden')
+
+  event.currentTarget.textContent = divAppIsHidden ?
+    'Exibir a div#app 🧐' :
+    'Ocultar a div#app 🤓'
+}
+
+link.addEventListener('click', handleToggleVisibilityDivApp)
