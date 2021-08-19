22130cda5935f2761d2e6f0a3479b9eedc9e8635
Gabriel Ramos <dev.gabrielramos@gmail.com>
2021-08-18 21:46:33 -0300

    Dia 02 - Resolução do exercício 02

--- dia2/src/calculator.js
+++ dia2/src/calculator.js
@@ -1,5 +1,4 @@
-function calculadora (callback) {
-}
+const calculadora = callback => (a, b) => callback(a, b)
 
 const sum = calculadora((a, b) => a + b)
 const sub = calculadora((a, b) => a - b)
@@ -9,4 +8,4 @@ const div = calculadora((a, b) => a / b)
 console.log('Somar 1 e 2 = 3:', sum(1, 2) === 3)
 console.log('Subtrair 4 de 20 = 16:', sub(20, 4) === 16)
 console.log('Multiplicar 3 com 3 = 9:', mult(3, 3) === 9)
-console.log('Dividir 15 por 5 = 3:', mult(15, 5) === 3)
+console.log('Dividir 15 por 5 = 3:', div(15, 5) === 3)
