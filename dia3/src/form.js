b418f342c09d4b3d3e262721942f23ef40bb0147
Gabriel Ramos <dev.gabrielramos@gmail.com>
2021-08-19 22:48:44 -0300

    Dia 03 - Resolução do exercício 01

---
+++ dia3/src/form.js
@@ -0,0 +1,36 @@
+const inputUsername = document.querySelector('[data-js="username"]')
+
+const paragraphFormattedUsername = document.createElement('output')
+
+const getSplittedUsername = username => username.split(' ')
+
+const getFormattedUsername = names => names
+  .map(name => /^de$|^da$|^do$|^dos$/.test(name)
+    ? name
+    : [name[0].toUpperCase(), ...name.slice(1)].join(''))
+  .join(' ')
+
+const insertParagraphFormattedUsernameIntoDOM = formattedUsername => {
+  paragraphFormattedUsername.textContent = formattedUsername
+  paragraphFormattedUsername.style.display = 'block'
+  inputUsername.insertAdjacentElement('afterend', paragraphFormattedUsername)
+}
+
+const showFormattedUsername = username => {
+  if (username) {
+    const splittedUsername = getSplittedUsername(username)
+    const formattedUsername = getFormattedUsername(splittedUsername)
+
+    insertParagraphFormattedUsernameIntoDOM(formattedUsername)
+    return
+  }
+
+  insertParagraphFormattedUsernameIntoDOM('')
+}
+
+const handleChangeUsername = event => {
+  const username = event.target.value.toLowerCase().trim()
+  showFormattedUsername(username)
+}
+
+inputUsername.addEventListener('input', handleChangeUsername)
