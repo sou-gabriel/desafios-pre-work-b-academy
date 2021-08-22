8d660937202dd152396540c6a0d22182d7e7c30f
Gabriel Ramos <dev.gabrielramos@gmail.com>
2021-08-22 01:04:20 -0300

    Dia 04 - Resolução do exercício 02

--- dia4/app/main.js
+++ dia4/app/main.js
@@ -1,1 +1,29 @@
 import './style.css'
+
+const tableContents = document.querySelector('[data-js="table-contents"]')
+
+const init = async () => {
+  try {
+    const response = await fetch('http://localhost:3333/cars')
+    const data = await response.json()
+
+    if (!data.length) {
+      const warningLine = document.createElement('tr')
+      const warningLineCell = document.createElement('td')
+
+      warningLine.setAttribute('data-js', 'warning-line')
+
+      warningLineCell.colSpan = 5
+      warningLineCell.textContent = 'Nenhum carro encontrado'
+      warningLineCell.style.textAlign = 'center'
+
+      warningLine.prepend(warningLineCell)
+      tableContents.prepend(warningLine)
+      return
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+init()
