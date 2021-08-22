9ceee8b426ecac48c5049a2355b9b180fbc241f4
Gabriel Ramos <dev.gabrielramos@gmail.com>
2021-08-22 10:16:44 -0300

    Dia 04 - Resolução do exercício 04

--- dia4/app/main.js
+++ dia4/app/main.js
@@ -4,12 +4,56 @@ const carForm = document.querySelector('[data-js="car-form"]')
 const tableContents = document.querySelector('[data-js="table-contents"]')
 const divFeedbackMessage = document.querySelector('[data-js="feedback"]')
 
+const createTableRow = data => {
+  const tableRow = document.createElement('tr')
+  const deleteButtonCell = document.createElement('th')
+  const button = document.createElement('button')
+
+  Object.values(data).forEach(cellValue => {
+    const tableData = document.createElement('td')
+
+    tableData.textContent = cellValue
+    tableRow.appendChild(tableData)
+  })
+
+  button.textContent = 'Excluir'
+
+  button.dataset.plate = data.plate
+  tableRow.dataset.plate = data.plate
+
+  button.addEventListener('click', async event => {
+    const plate = event.target.dataset.plate
+
+    const response = await fetch('http://localhost:3333/cars', {
+      method: 'DELETE',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({ plate })
+    })
+
+    if (!response.ok) {
+      console.log('Um erro ocorreu')
+      return
+    }
+
+    const tableRow = document.querySelector(`tr[data-plate="${plate}"]`)
+    tableRow.remove()
+  })
+  
+  deleteButtonCell.append(button)
+  tableRow.appendChild(deleteButtonCell)
+
+  tableContents.appendChild(tableRow)
+}
+
 const init = async () => {
   try {
     const response = await fetch('http://localhost:3333/cars')
-    const data = await response.json()
+    const carData = await response.json()
 
-    if (!data.length) {
+    if (!carData.length) {
+      console.log('opa')
       const warningLine = document.createElement('tr')
       const warningLineCell = document.createElement('td')
 
@@ -24,22 +68,7 @@ const init = async () => {
       return
     }
 
-    data
-      .map(car => {
-        const tableRow = document.createElement('tr')
-
-        Object.values(car).forEach(feature => {
-          const tableData = document.createElement('td')
-
-          tableData.textContent = feature
-          tableRow.append(tableData)
-        })
-
-        return tableRow
-      })
-      .forEach(tableRow => {
-        tableContents.append(tableRow)
-      })
+    carData.forEach(createTableRow)
   } catch (error) {
     divFeedbackMessage.textContent = error.message    
     divFeedbackMessage.classList.remove('success')
@@ -49,14 +78,6 @@ const init = async () => {
 
 carForm.addEventListener('submit', async event => {
   event.preventDefault()
-
-  const carFeatures = [
-    event.target.image.value,
-    event.target.brandModel.value,
-    event.target.year.valueAsNumber,
-    event.target.plate.value,
-    event.target.color.value
-  ]
 
   try {
     const data = await (await fetch('http://localhost:3333/cars', {
@@ -87,20 +108,15 @@ carForm.addEventListener('submit', async event => {
       warningLineExists.remove()
     }
 
-    const tableRow = document.createElement('tr')
-    
-    carFeatures
-      .map(feature => {
-        const tableData = document.createElement('td')
+    const carData = {
+      image: event.target.image.value,
+      brandModel: event.target.brandModel.value,
+      year: event.target.year.valueAsNumber,
+      plate: event.target.plate.value,
+      color: event.target.color.value
+    }
 
-        tableData.textContent = feature
-        return tableData
-      })
-      .forEach(cell => {
-        tableRow.append(cell)
-      })
-
-    tableContents.append(tableRow)
+    createTableRow(carData)
   } catch (error) {
     divFeedbackMessage.textContent = error.message
     divFeedbackMessage.classList.remove('success')
