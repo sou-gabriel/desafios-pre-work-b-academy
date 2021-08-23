59f544c1a1817eb2c0dc344572821ac2d4b196ad
Gabriel Ramos <dev.gabrielramos@gmail.com>
2021-08-22 22:23:02 -0300

    Dia 05 - Implementa tipagem nos identificadores da 'http.ts'

--- dia5/app/src/http.ts
+++ dia5/app/src/http.ts
@@ -1,16 +1,25 @@
-const request = (url, options) =>
+type Options = {
+  method: string
+  headers: {
+    'content-type': string
+  }
+  body: string
+}
+
+const request = (url: string, options?: Options) =>
   fetch(url, options)
     .then(r => r.json())
     .catch(e => ({ error: true, message: e.message }))
 
-const createRequest = (method) => (url, data) => request(url, {
-  method,
-  headers: {
-    'content-type': 'application/json',
-  },
-  body: JSON.stringify(data)
-})
+const createRequest = (method: string) => <T>(url: string, data: T) =>
+  request(url, {
+    method,
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify(data)
+  })
 
-export const get = (url) => request(url)
+export const get = (url: string) => request(url)
 export const post = createRequest('POST')
 export const del = createRequest('DELETE')
